8a1c633996f5237ed644b3d2bb40817db11e03a9
ableshih <ableshih@gmail.com>
2018-02-01 22:12:48 +0800

    Update dir.py

--- code/dir.py
+++ code/dir.py
@@ -3,7 +3,9 @@
 要不然的話就直接開檔案
 """
 import os
-path = "d:\\123"
+path = "d:\\123456"
 if not os.path.isdir(path):
     os.mkdir(path)
-file = open(path + "\\" + "我要開檔案.txt", "w")
+    file = open(path + "\\" + "我要開檔案.txt", "w")
+else:
+    print("is ok")
